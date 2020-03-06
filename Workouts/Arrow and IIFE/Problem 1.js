35878de675f33020f620813ceb8db49380977142
David <david.a.25897@gmail.com>
2020-03-06 12:32:59 +0530

    Update Problem 1.js

--- Workouts/Arrow and IIFE/Problem 1.js
+++ Workouts/Arrow and IIFE/Problem 1.js
@@ -1,14 +1,10 @@
 (function() { 
 b=[];
-c=[];
 
 for(i=0;i<a.length;i++){
-	if(i%2==0){
+	if(i%2==1){
 	b.push(i);
 	}
-	else{
-	c.push(i);
-	}
 }
-console.log(c);
+console.log(b);
 })(a=[1,2,3,4,5,6,7,8,9,10,11,12,13]);
