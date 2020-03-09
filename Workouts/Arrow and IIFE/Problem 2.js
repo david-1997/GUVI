cbece3d603104f2634751854a6b2bf40f3678831
David <david.a.25897@gmail.com>
2020-03-09 19:09:54 +0530

    Add files via upload

---
+++ Workouts/Arrow and IIFE/Problem 2.js
@@ -0,0 +1,7 @@
+(function (string) {
+      var sentence = string.toLowerCase().split(" ");
+      for(var i = 0; i< sentence.length; i++){
+         sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+}
+console.log(sentence.join(" "));
+})("iron man")
