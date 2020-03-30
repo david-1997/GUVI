b5b21844e932fa08f52fccdaf02edd258cda250f
David <david.a.25897@gmail.com>
2020-03-31 01:57:01 +0530

    Update index.js

--- Workouts/NodeJS & MongoDB/Password Hashing/index.js
+++ Workouts/NodeJS & MongoDB/Password Hashing/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 var cors = require('cors')
 const app = express();
 const MongoClient = require('mongodb');
@@ -28,6 +28,32 @@ app.post('/register', function (req, res) {
         })
     })
 })
+
+app.post('/login', function (req, res) {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+        if (err) throw err;
+        var db = client.db("DB");
+        db.collection('login').findOne({ email: req.body.email }, (err, user) => {
+            if (err) throw err;
+            console.log(user.email)
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                if (err) throw err;
+                client.close();
+                if (result) {
+                    res.json({
+                        message: "success"
+                    })
+                }
+                else{
+                    res.json({
+                        message: "failure"
+                    })
+                }
+            })
+        })
+    })
+});
+
 app.listen(3030, function () {
     console.log("Port is running on 3030")
 });
